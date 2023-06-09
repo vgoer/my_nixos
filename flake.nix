c514547e53a1c6bf81f16e000bb5063894021f9a
vgoer <hi_goer@163.com>
2023-06-09 14:35:36 +0800

    goer:nixos配置

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Goer's nix config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager,  ... }:
+  let
+    system = "x86_64-linux";
+  in
+  {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./nixos/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useUserPackages = true;
+              useGlobalPkgs = true;
+              users.goer = ./home-manager/home.nix;
+            };
+          }
+        ];
+      };
+    };
+  };
+}
+
---
+++ home-manager/apps/micro.nix
@@ -0,0 +1,15 @@
+{
+  programs.micro = {
+    enable = true;
+    settings = {
+      colorscheme = "material-tc";
+      mkparents = true;
+      softwrap = true;
+      tabmovement = true;
+      tabsize = 2;
+      tabstospaces = true;
+      autosu = true;
+    };
+  };
+}
+
